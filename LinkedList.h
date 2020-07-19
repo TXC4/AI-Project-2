85d57edc464d66725d1b18a07c49279ea8cc3d67
TXC4 <43871212+TXC4@users.noreply.github.com>
2020-07-18 21:38:36 -0500

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,24 +1,48 @@
 #pragma once
 #include <string>
+#include <vector>
 
-struct List
+
+
+struct Node
 {
-	struct Node
-	{	
-		Node(std::string _city, Node* _adjacentCity1 = nullptr, Node* _adjacentCity2 = nullptr,
-			Node* _adjacentCity3 = nullptr, Node* _adjacentCity4 = nullptr)
-		{
-			city = _city;
-			if (_adjacentCity1 != nullptr)
-				Node* adjacentCity1 = _adjacentCity1;
-			if (_adjacentCity2 != nullptr)
-				Node* adjacentCity2 = _adjacentCity2;
-			if (_adjacentCity3 != nullptr)
-				Node* adjacentCity3 = _adjacentCity3;
-			if (_adjacentCity4 != nullptr)
-				Node* adjacentCity4 = _adjacentCity4;
-		}
-		std::string city = "";
-		Node* adjacentCity;
+	struct Cxn
+	{
+		Node* adjCity;
+		int distance = 0;
 	};
+	std::string city = "";
+	std::vector<Cxn>cxnList;
+	Node(std::string _city)
+	{
+		city = _city;
+	}
+	void addCxn(Node* newNode1 = nullptr, int d1 = 0, Node* newNode2 = nullptr, int d2 = 0,
+		Node* newNode3 = nullptr, int d3 = 0, Node* newNode4 = nullptr, int d4 = 0)
+	{
+		if (newNode1 != nullptr)
+		{
+			Cxn* newCxn1 = new Cxn;
+			newCxn1->adjCity = newNode1;
+			newCxn1->distance = d1;
+		}
+		if (newNode2 != nullptr)
+		{
+			Cxn* newCxn2 = new Cxn;
+			newCxn2->adjCity = newNode2;
+			newCxn2->distance = d2;
+		}
+		if (newNode3 != nullptr)
+		{
+			Cxn* newCxn3 = new Cxn;
+			newCxn3->adjCity = newNode3;
+			newCxn3->distance = d3;
+		}
+		if (newNode4 != nullptr)
+		{
+			Cxn* newCxn4 = new Cxn;
+			newCxn4->adjCity = newNode4;
+			newCxn4->distance = d4;
+		}
+	}
 };
