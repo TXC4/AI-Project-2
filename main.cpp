a15b471777f9483c59d997a2bd303dde199601a8
TXC4 <43871212+TXC4@users.noreply.github.com>
2020-07-18 21:38:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
+#include "LinkedList.h"
 
+using namespace std;
+
+int main()
+{
+	Node Oradea("Oradea");
+	Node Zerind("Zerind");
+	Node Arad("Arad");
+	Oradea.addCxn(&Zerind, 71);
+	Zerind.addCxn(&Oradea, 71);
+	Zerind.addCxn(&Arad, 75);
+}
