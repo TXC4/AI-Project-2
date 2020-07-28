d7b769ada5a958ce6f529e695d570a8714b01d04
TXC4 <43871212+TXC4@users.noreply.github.com>
2020-07-28 11:17:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,99 +17,105 @@ void IDS(Node*);
 
 int main()
 {
-	Node Oradea("Oradea");
-	Node Zerind("Zerind");
-	Node Arad("Arad");
-	Node Timisoara("Timisoara");
-	Node Lugoj("Lugoj");
-	Node Mehadia("Mehadia");
-	Node Drobeta("Drobeta");
-	Node Sibiu("Sibiu");
-	Node Fagarus("Fagarus");
-	Node RV("Rimnicu Vilcea");
-	Node Pitesti("Pitesti");
-	Node Craiova("Craiova");
-	Node Bucharest("Bucharest");
-	Node Giurgiu("Giurgiu");
-	Node Urziceni("Urziceni"); 
-	Node Vaslui("Vaslui");
-	Node Iasi("Iasi");
-	Node Neamt("Neamt");
-	Node Hirsova("Hirsova");
-	Node Eforie("Eforie");
+	cout << "DISCLAIMER:  Known bug when running search multiple times without closing program\n\n";
+	while (true)
+	{
+		Node Oradea("Oradea");
+		Node Zerind("Zerind");
+		Node Arad("Arad");
+		Node Timisoara("Timisoara");
+		Node Lugoj("Lugoj");
+		Node Mehadia("Mehadia");
+		Node Drobeta("Drobeta");
+		Node Sibiu("Sibiu");
+		Node Fagarus("Fagarus");
+		Node RV("Rimnicu Vilcea");
+		Node Pitesti("Pitesti");
+		Node Craiova("Craiova");
+		Node Bucharest("Bucharest");
+		Node Giurgiu("Giurgiu");
+		Node Urziceni("Urziceni"); 
+		Node Vaslui("Vaslui");
+		Node Iasi("Iasi");
+		Node Neamt("Neamt");
+		Node Hirsova("Hirsova");
+		Node Eforie("Eforie");
 
-	Oradea.addCxn(&Zerind, 71, &Sibiu, 151);
-	Zerind.addCxn(&Oradea, 71, &Arad, 75);
-	Arad.addCxn(&Zerind, 75, &Sibiu, 151, &Timisoara, 118);
-	Timisoara.addCxn(&Arad, 118, &Lugoj, 111);
-	Lugoj.addCxn(&Timisoara, 111, &Mehadia, 70);
-	Mehadia.addCxn(&Lugoj, 70, &Drobeta, 75);
-	Drobeta.addCxn(&Mehadia, 75, &Craiova, 120);
-	Craiova.addCxn(&Drobeta, 120, &RV, 146, &Pitesti, 138);
-	RV.addCxn(&Sibiu, 80, &Craiova, 146, &Pitesti, 101);
-	Sibiu.addCxn(&Oradea, 151, &Arad, 140, &RV, 80, &Fagarus, 99);
-	Fagarus.addCxn(&Sibiu, 99, &Bucharest, 211);
-	Pitesti.addCxn(&RV, 97, &Craiova, 138, &Bucharest, 101);
-	Bucharest.addCxn(&Pitesti, 101, &Fagarus, 211, &Giurgiu, 90, &Urziceni, 85);
-	Giurgiu.addCxn(&Bucharest, 90);
-	Urziceni.addCxn(&Bucharest, 85, &Hirsova, 98, &Vaslui, 142);
-	Hirsova.addCxn(&Urziceni, 98, &Eforie, 86);
-	Eforie.addCxn(&Hirsova, 86);
-	Vaslui.addCxn(&Urziceni, 142, &Iasi, 92);
-	Iasi.addCxn(&Vaslui, 92, &Neamt, 87);
-	Neamt.addCxn(&Iasi, 92);
+		Oradea.addCxn(&Zerind, 71, &Sibiu, 151);
+		Zerind.addCxn(&Oradea, 71, &Arad, 75);
+		Arad.addCxn(&Zerind, 75, &Sibiu, 151, &Timisoara, 118);
+		Timisoara.addCxn(&Arad, 118, &Lugoj, 111);
+		Lugoj.addCxn(&Timisoara, 111, &Mehadia, 70);
+		Mehadia.addCxn(&Lugoj, 70, &Drobeta, 75);
+		Drobeta.addCxn(&Mehadia, 75, &Craiova, 120);
+		Craiova.addCxn(&Drobeta, 120, &RV, 146, &Pitesti, 138);
+		RV.addCxn(&Sibiu, 80, &Craiova, 146, &Pitesti, 101);
+		Sibiu.addCxn(&Oradea, 151, &Arad, 140, &RV, 80, &Fagarus, 99);
+		Fagarus.addCxn(&Sibiu, 99, &Bucharest, 211);
+		Pitesti.addCxn(&RV, 97, &Craiova, 138, &Bucharest, 101);
+		Bucharest.addCxn(&Pitesti, 101, &Fagarus, 211, &Giurgiu, 90, &Urziceni, 85);
+		Giurgiu.addCxn(&Bucharest, 90);
+		Urziceni.addCxn(&Bucharest, 85, &Hirsova, 98, &Vaslui, 142);
+		Hirsova.addCxn(&Urziceni, 98, &Eforie, 86);
+		Eforie.addCxn(&Hirsova, 86);
+		Vaslui.addCxn(&Urziceni, 142, &Iasi, 92);
+		Iasi.addCxn(&Vaslui, 92, &Neamt, 87);
+		Neamt.addCxn(&Iasi, 92);
 
-	string sl = getStartLocation();
-	string st = getSearchType();
+	
+		cout << endl;
+		string sl = getStartLocation();
+		string st = getSearchType();
 
-	Node* startNode = nullptr;
-	if (sl == "Oradea")
-		startNode = &Oradea;
-	else if (sl == "Zerind")
-		startNode = &Zerind;
-	else if (sl == "Arad")
-		startNode = &Arad;
-	else if (sl == "Timisoara")
-		startNode = &Timisoara;
-	else if (sl == "Lugoj")
-		startNode = &Lugoj;
-	else if (sl == "Mehadia")
-		startNode = &Mehadia;
-	else if (sl == "Drobeta")
-		startNode = &Drobeta;
-	else if (sl == "Craiova")
-		startNode = &Craiova;
-	else if (sl == "Remnicu Vilcea")
-		startNode = &RV;
-	else if (sl == "Sibiu")
-		startNode = &Sibiu;
-	else if (sl == "Fagarus")
-		startNode = &Fagarus;
-	else if (sl == "Pitesti")
-		startNode = &Pitesti;
-	else if (sl == "Bucharest")
-		startNode = &Bucharest;
-	else if (sl == "Giurgiu")
-		startNode = &Giurgiu;
-	else if (sl == "Urziceni")
-		startNode = &Urziceni;
-	else if (sl == "Hirsova")
-		startNode = &Hirsova;
-	else if (sl == "Eforie")
-		startNode = &Eforie;
-	else if (sl == "Vaslui")
-		startNode = &Vaslui;
-	else if (sl == "Iasi")
-		startNode = &Iasi;
-	else if (sl == "Neamt")
-		startNode = &Neamt;
+		Node* startNode = nullptr;
+		if (sl == "Oradea")
+			startNode = &Oradea;
+		else if (sl == "Zerind")
+			startNode = &Zerind;
+		else if (sl == "Arad")
+			startNode = &Arad;
+		else if (sl == "Timisoara")
+			startNode = &Timisoara;
+		else if (sl == "Lugoj")
+			startNode = &Lugoj;
+		else if (sl == "Mehadia")
+			startNode = &Mehadia;
+		else if (sl == "Drobeta")
+			startNode = &Drobeta;
+		else if (sl == "Craiova")
+			startNode = &Craiova;
+		else if (sl == "Remnicu Vilcea")
+			startNode = &RV;
+		else if (sl == "Sibiu")
+			startNode = &Sibiu;
+		else if (sl == "Fagarus")
+			startNode = &Fagarus;
+		else if (sl == "Pitesti")
+			startNode = &Pitesti;
+		else if (sl == "Bucharest")
+			startNode = &Bucharest;
+		else if (sl == "Giurgiu")
+			startNode = &Giurgiu;
+		else if (sl == "Urziceni")
+			startNode = &Urziceni;
+		else if (sl == "Hirsova")
+			startNode = &Hirsova;
+		else if (sl == "Eforie")
+			startNode = &Eforie;
+		else if (sl == "Vaslui")
+			startNode = &Vaslui;
+		else if (sl == "Iasi")
+			startNode = &Iasi;
+		else if (sl == "Neamt")
+			startNode = &Neamt;
 
-	if (st == "b")
-		BFS(startNode);
-	else if (st == "d")
-		DFS(startNode);
-	else if (st == "i")
-		IDS(startNode);
+		if (st == "b")
+			BFS(startNode);
+		else if (st == "d")
+			DFS(startNode);
+		else if (st == "i")
+			IDS(startNode);
+	}
 }
 
 string getStartLocation()
@@ -268,6 +274,8 @@ void DFS(Node* root)
 			if (!i.adjCity->visited)
 			{
 				dStack.push_back(i.adjCity);
+				///////////
+				vList.push_back(i.adjCity->city);
 
 				std::ofstream oStr;
 				oStr.open("visited.txt");
@@ -373,4 +381,6 @@ void IDS(Node* root)
 	}
 	oStr2.close();
 	cout << endl;
+	pathList.clear();
+	visitedList.clear();
 }
